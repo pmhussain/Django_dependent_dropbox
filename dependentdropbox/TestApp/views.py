92c21376f4607c1cc1855bd4cd494bf7cb4b069a
pmhussain <e2academy2020@gmail.com>
2024-02-28 10:29:55 +0530

    drop down WITHOUT Ajax is implemented with one view function, one url, one html file usingGET method

--- dependentdropbox/TestApp/views.py
+++ dependentdropbox/TestApp/views.py
@@ -32,3 +32,28 @@ def loaddistricts(request):
     'districts' : districts,
     }
     return render (request, 'districts_dropdown_list.html', context)
+
+
+
+#Without AJAX
+def withoutajax(request):
+    countries = Country.objects.all()
+    country_id = request.GET.get('country_id', None)
+    state_id = request.GET.get('state_id', None)
+    states = None
+    districts = None
+    print(locals())
+    if country_id:
+        get_country = Country.objects.get(id=country_id)
+        states = State.objects.filter(country=get_country)
+        print(states)
+    if state_id:
+        get_state = State.objects.get(id=state_id)
+        districts = District.objects.filter(state=get_state)
+        print(districts)
+    context = {
+    'countries' : countries,
+    'states' : states,
+    'districts' : districts,
+    }
+    return render (request, 'withoutajax.html',locals())
